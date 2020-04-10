91b7f79dd19fc2862239eb3d499d1110534ba577
Gabriel Engel <gfbengel@gmail.com>
2020-04-10 19:40:13 -0300

    projeto

---
+++ src/index.js
@@ -0,0 +1,105 @@
+const express = require('express')
+const { uuid, isUuid } = require('uuidv4')
+
+const app = express()
+app.use(express.json())
+
+const projects = []
+
+function logRequests(request, response, next) {
+  const { method, url } = request
+
+  const logLabel = `[${method.toUpperCase()}] ${url}`
+
+  console.time(logLabel)
+
+  next()
+
+  console.timeEnd(logLabel)
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params
+
+  if (!isUuid(id)) {
+    return response.status(400).json({
+      error: 'Invalid project ID.'
+    })
+  }
+
+  return next()
+}
+
+app.use(logRequests)
+
+app.use('/projects/:id', validateProjectId)
+
+app.get('/', (request, response) => {
+  response.status(200).json({
+    'success': true,
+    'msg': 'Hello world',
+  })
+})
+
+app.get('/projects', (request, response) => {
+  const { name } = request.query
+
+  const results = name
+    ? projects.filter(project => project.name.includes(name))
+    : projects
+
+  return response.json(results)
+})
+
+app.post('/projects', (request, response) => {
+  const { name } = request.body
+
+  const project = { id: uuid(), name }
+
+  projects.push(project)
+
+  return response.json(project)
+
+})
+
+app.put('/projects/:id', (request, response) => {
+  const { id } = request.params
+  const { name } = request.body
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({
+      error: 'Proejct not found.'
+    })
+  }
+
+  const project = { id, name }
+
+
+  projects[projectIndex] = project
+
+  return response.json(project)
+
+})
+
+app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+
+  if (projectIndex < 0) {
+    return response.status(400).json({
+      error: 'Proejct not found.'
+    })
+  }
+
+  projects.splice(projectIndex, 1)
+
+  return response.status(204).send()
+
+})
+
+app.listen(3333, () => {
+  console.log('Backend started!')
+})
