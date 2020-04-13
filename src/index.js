3143749584affad44d0a0cbe4bb1b962c461631a
Gabriel Engel <gfbengel@gmail.com>
2020-04-13 20:35:04 -0300

    adicionando cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = requier('cors')
 const { uuid, isUuid } = require('uuidv4')
 
 const app = express()
+app.use(cors())
+
 app.use(express.json())
 
 const projects = []
